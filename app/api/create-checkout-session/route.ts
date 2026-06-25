73f6138fa93424478ecd5af83ab4fbfecdff7378
gabrielbzago <gabrielbzago@gmail.com>
2026-06-25 15:18:47 -0300

    Add files via upload

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -3,10 +3,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY!,
-  {
-    apiVersion: "2025-04-30.basil",
-  }
+  process.env.STRIPE_SECRET_KEY!
 );
 
 export async function POST(
